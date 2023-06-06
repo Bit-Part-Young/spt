5bf7ffd31bbcda44b0790a1d4ee46f8335028a67
yangsl306 <3242809681@qq.com>
2023-06-06 22:59:36 +0800

    test script and figure

---
+++ plot.py
@@ -0,0 +1,19 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from plot_params import set_plot_params
+
+
+x = np.linspace(0, 2*np.pi, 100)
+y = np.sin(x)
+
+set_plot_params()
+
+fig, ax = plt.subplots()
+
+ax.plot(x, y, label='$y=\sin(x)$')
+
+ax.set_xlabel('x')
+ax.set_ylabel('y')
+ax.legend()
+
+fig.savefig('sin.png')
