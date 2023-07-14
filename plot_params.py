2bb77313282525485344aa9e78e04560f0b28f1b
YSL <3242809681@qq.com>
2023-07-14 11:31:17 +0800

    update mathtext font

--- plot_params.py
+++ plot_params.py
@@ -8,7 +8,7 @@ def set_plot_params(
     title_weight="heavy",
     font_size=12,
     legend_fontsize=12,
-    math_font="regular",
+    math_font="stix",
     xtick_dir="in",
     ytick_dir="in",
     xtick_width=1.5,
@@ -33,15 +33,15 @@ def set_plot_params(
 
     Parameters
     ----------
-    font_family : str, optional  字体类型, 默认值为 'serif'.
-    font_weight : str, optional  字体加粗类型, 默认值为 'heavy'.
-    label_weight : str, optional  坐标轴标签加粗类型, 默认值为 'heavy'.
-    title_weight : str, optional  图表标题加粗类型, 默认值为 'heavy'.
+    font_family : str, optional  字体类型, 默认值为 "serif".
+    font_weight : str, optional  字体加粗类型, 默认值为 "heavy".
+    label_weight : str, optional  坐标轴标签加粗类型, 默认值为 "heavy".
+    title_weight : str, optional  图表标题加粗类型, 默认值为 "heavy".
     font_size : int, optional  字体大小, 默认值为 12.
     legend_fontsize: int, optional  图例字体大小, 默认值为 12.
-    math_font : str, optional  数学公式字体类型, 默认值为 'regular', 即非斜体.
-    xtick_dir : str, optional  x 轴主刻度线方向, 默认值为 'in'.
-    ytick_dir : str, optional  y 轴主刻度线方向, 默认值为 'in'.
+    math_font : str, optional  数学公式字体类型, 默认值为 "stix", 即斜体.
+    xtick_dir : str, optional  x 轴主刻度线方向, 默认值为 "in".
+    ytick_dir : str, optional  y 轴主刻度线方向, 默认值为 "in".
     xtick_width : float, optional  x 轴主刻度线宽度, 默认值为 1.5.
     ytick_width : float, optional  y 轴主刻度线宽度, 默认值为 1.5.
     axis_width : float, optional  坐标轴线宽度, 默认值为 1.5.
@@ -57,20 +57,21 @@ def set_plot_params(
     title_pad : int, optional  标题与坐标轴的距离, 默认值为 10.
     savefig_dpi : int, optional  图片保存时的分辨率, 默认值为 300.
     figure_dpi : int, optional  图片显示时的分辨率, 默认值为 300.
-    savefig_bbox : str, optional  图片保存时的裁剪方式, 默认值为 'tight', 即裁剪掉图片周围的空白部分.
+    savefig_bbox : str, optional  图片保存时的裁剪方式, 默认值为 "tight", 即裁剪掉图片周围的空白部分.
     -------
     Returns:  None
 
     其他可以补充的plt.rcParams
-    'axes.grid': True/False, 设置是否显示坐标轴网格线.
-    'axes.edgecolor': 'color', 设置坐标轴边框的颜色.
-    'axes.facecolor': 'color', 设置坐标轴背景颜色.
-    'xtick.labelsize': size, 设置x轴刻度标签的字体大小.
-    'ytick.labelsize': size, 设置y轴刻度标签的字体大小.
-    'lines.linewidth': width, 设置线条宽度.
-    'lines.markersize': size, 设置标记的大小.
-    'text.usetex': True/False, 设置是否使用TeX解析字符串中的LaTeX标记.
-    'axes.prop_cycle': cycle, 设置默认颜色循环.
+    "axes.grid": True/False, 设置是否显示坐标轴网格线.
+    "axes.edgecolor": "color", 设置坐标轴边框的颜色.
+    "axes.facecolor": "color", 设置坐标轴背景颜色.
+    "xtick.labelsize": size, 设置x轴刻度标签的字体大小.
+    "ytick.labelsize": size, 设置y轴刻度标签的字体大小.
+    "lines.linewidth": width, 设置线条宽度.
+    "lines.markersize": size, 设置标记的大小.
+    "text.usetex": True/False, 设置是否使用TeX解析字符串中的LaTeX标记.
+    "axes.prop_cycle": cycle, 设置默认颜色循环.
+    "mathtext.default": 数学公式字体类型, "regular"为非斜体.
     """
 
     # 创建包含参数名称和对应值的字典
@@ -81,7 +82,7 @@ def set_plot_params(
         "axes.titleweight": title_weight,
         "font.size": font_size,
         "legend.fontsize": legend_fontsize,
-        "mathtext.default": math_font,
+        "mathtext.fontset": math_font,
         "xtick.direction": xtick_dir,
         "ytick.direction": ytick_dir,
         "xtick.major.width": xtick_width,
