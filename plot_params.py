0de9f303faec4b65eaba87a149b9bd4f8dd236ec
YSL <3242809681@qq.com>
2023-07-17 17:15:28 +0800

    modify params dict keys

--- plot_params.py
+++ plot_params.py
@@ -4,26 +4,26 @@ import matplotlib.pyplot as plt
 def set_plot_params(
     font_family="serif",
     font_weight="heavy",
-    label_weight="heavy",
-    title_weight="heavy",
+    axes_labelweight="heavy",
+    axes_titleweight="heavy",
     font_size=12,
     legend_fontsize=12,
-    math_font="stix",
-    xtick_dir="in",
-    ytick_dir="in",
-    xtick_width=1.5,
-    ytick_width=1.5,
-    axis_width=1.5,
+    mathtext_fontset="stix",
+    xtick_direction="in",
+    ytick_direction="in",
+    xtick_major_width=1.5,
+    ytick_major_width=1.5,
+    axes_linewidth=1.5,
     xtick_top=False,
     ytick_right=False,
     xtick_minor_visible=False,
     ytick_minor_visible=False,
     xtick_minor_width=1.0,
     ytick_minor_width=1.0,
-    xtick_pad=7,
-    ytick_pad=7,
-    label_pad=10,
-    title_pad=10,
+    xtick_major_pad=7,
+    ytick_major_pad=7,
+    axes_labelpad=10,
+    axes_titlepad=10,
     savefig_dpi=300,
     figure_dpi=300,
     savefig_bbox="tight",
@@ -35,30 +35,32 @@ def set_plot_params(
     ----------
     font_family : str, optional  字体类型, 默认值为 "serif".
     font_weight : str, optional  字体加粗类型, 默认值为 "heavy".
-    label_weight : str, optional  坐标轴标签加粗类型, 默认值为 "heavy".
-    title_weight : str, optional  图表标题加粗类型, 默认值为 "heavy".
+    axes_labelweight : str, optional  坐标轴标签加粗类型, 默认值为 "heavy".
+    axes_titleweight : str, optional  图表标题加粗类型, 默认值为 "heavy".
     font_size : int, optional  字体大小, 默认值为 12.
     legend_fontsize: int, optional  图例字体大小, 默认值为 12.
-    math_font : str, optional  数学公式字体类型, 默认值为 "stix", 即斜体.
-    xtick_dir : str, optional  x 轴主刻度线方向, 默认值为 "in".
-    ytick_dir : str, optional  y 轴主刻度线方向, 默认值为 "in".
-    xtick_width : float, optional  x 轴主刻度线宽度, 默认值为 1.5.
-    ytick_width : float, optional  y 轴主刻度线宽度, 默认值为 1.5.
-    axis_width : float, optional  坐标轴线宽度, 默认值为 1.5.
+    mathtext_fontset : str, optional  数学公式字体类型, 默认值为 "sitx", 即斜体.
+    xtick_direction : str, optional  x 轴主刻度线方向, 默认值为 "in".
+    ytick_direction : str, optional  y 轴主刻度线方向, 默认值为 "in".
+    xtick_major_width : float, optional  x 轴主刻度线宽度, 默认值为 1.5.
+    ytick_major_width : float, optional  y 轴主刻度线宽度, 默认值为 1.5.
+    axes_linewidth : float, optional  坐标轴线宽度, 默认值为 1.5.
     xtick_top : bool, optional  是否在 x 轴顶部添加刻度线, 默认值为 False.
     ytick_right : bool, optional  是否在 y 轴右侧添加刻度线, 默认值为 False.
     xtick_minor_visible : bool, optional  是否添加 x 轴次刻度线, 默认值为 False.
     ytick_minor_visible : bool, optional  是否添加 y 轴次刻度线, 默认值为 False.
     xtick_minor_width : float, optional  x 轴次刻度线宽度, 默认值为 1.0.
     ytick_minor_width : float, optional  y 轴次刻度线宽度, 默认值为 1.0.
-    xtick_pad : int, optional  x 轴主刻度线标签与刻度线的距离, 默认值为 7.
-    ytick_pad : int, optional  y 轴主刻度线标签与刻度线的距离, 默认值为 7.
-    label_pad : int, optional  坐标轴标签与坐标轴的距离, 默认值为 10.
-    title_pad : int, optional  标题与坐标轴的距离, 默认值为 10.
+    xtick_major_pad : int, optional  x 轴主刻度线标签与刻度线的距离, 默认值为 7.
+    ytick_major_pad : int, optional  y 轴主刻度线标签与刻度线的距离, 默认值为 7.
+    axes_labelpad : int, optional  坐标轴标签与坐标轴的距离, 默认值为 10.
+    axes_titlepad : int, optional  标题与坐标轴的距离, 默认值为 10.
     savefig_dpi : int, optional  图片保存时的分辨率, 默认值为 300.
     figure_dpi : int, optional  图片显示时的分辨率, 默认值为 300.
     savefig_bbox : str, optional  图片保存时的裁剪方式, 默认值为 "tight", 即裁剪掉图片周围的空白部分.
+
     -------
+    
     Returns:  None
 
     其他可以补充的plt.rcParams
@@ -78,26 +80,26 @@ def set_plot_params(
     params = {
         "font.family": font_family,
         "font.weight": font_weight,
-        "axes.labelweight": label_weight,
-        "axes.titleweight": title_weight,
+        "axes.labelweight": axes_labelweight,
+        "axes.titleweight": axes_titleweight,
         "font.size": font_size,
         "legend.fontsize": legend_fontsize,
-        "mathtext.fontset": math_font,
-        "xtick.direction": xtick_dir,
-        "ytick.direction": ytick_dir,
-        "xtick.major.width": xtick_width,
-        "ytick.major.width": ytick_width,
-        "axes.linewidth": axis_width,
+        "mathtext.fontset": mathtext_fontset,
+        "xtick.direction": xtick_direction,
+        "ytick.direction": ytick_direction,
+        "xtick.major.width": xtick_major_width,
+        "ytick.major.width": ytick_major_width,
+        "axes.linewidth": axes_linewidth,
         "xtick.top": xtick_top,
         "ytick.right": ytick_right,
         "xtick.minor.visible": xtick_minor_visible,
         "ytick.minor.visible": ytick_minor_visible,
         "xtick.minor.width": xtick_minor_width,
         "ytick.minor.width": ytick_minor_width,
-        "xtick.major.pad": xtick_pad,
-        "ytick.major.pad": ytick_pad,
-        "axes.labelpad": label_pad,
-        "axes.titlepad": title_pad,
+        "xtick.major.pad": xtick_major_pad,
+        "ytick.major.pad": ytick_major_pad,
+        "axes.labelpad": axes_labelpad,
+        "axes.titlepad": axes_titlepad,
         "savefig.dpi": savefig_dpi,
         "figure.dpi": figure_dpi,
         "savefig.bbox": savefig_bbox,
