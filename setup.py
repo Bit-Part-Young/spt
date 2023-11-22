a6704ede03f2de14f6da31d0c140c2a06d32c5bf
yangsl <3242809681@qq.com>
2023-11-22 22:39:43 +0800

    update setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,15 @@
-from setuptools import find_packages, setup
+from distutils.core import setup
+
+from setuptools import find_packages
 
 setup(
     name="spt",
-    version="0.1.3",
-    description="scientific matplotlib plot template python module",
+    version="0.1.4",
+    description="Scientific matplotlib plot template python module.",
     author="yangsl",
     author_email="3304839708@qq.com",
     url="https://gitee.com/yangsl306/spt",
     packages=find_packages(),
     zip_safe=False,
+    platforms=["all"],
 )
