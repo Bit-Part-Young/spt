332b97d076dde033264b53189aaf412dbda03dce
YSL <3242809681@qq.com>
2023-11-22 22:09:58 +0800

    update setup.py

--- setup.py
+++ setup.py
@@ -2,12 +2,11 @@ from setuptools import find_packages, setup
 
 setup(
     name="spt",
-    version="0.1.0",
+    version="0.1.3",
     description="scientific matplotlib plot template python module",
     author="yangsl",
     author_email="3304839708@qq.com",
     url="https://gitee.com/yangsl306/spt",
     packages=find_packages(),
-    install_requires=["matplotlib"],
     zip_safe=False,
 )
