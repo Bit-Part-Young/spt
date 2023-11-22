32900d9c21d9fc01c46bfc8c9251b36e323b4460
yangsl <3242809681@qq.com>
2023-11-22 23:51:11 +0800

    update setup.py

--- setup.py
+++ setup.py
@@ -1,16 +1,36 @@
 from distutils.core import setup
+from pathlib import Path
+from typing import List
 
 from setuptools import find_packages
 
+THIS_DIR = Path(__file__).parent
+
+with open(THIS_DIR / "README.md", encoding="utf-8") as f:
+    long_description = f.read()
+
+
+def read_requirements(filepath: Path) -> List[str]:
+    with open(filepath, encoding="utf-8") as fd:
+        return [
+            package.strip("\n")
+            for package in fd.readlines()
+            if not package.startswith("#")
+        ]
+
+requirements = read_requirements(THIS_DIR / "requirements.txt")
+
+
 setup(
     name="spt",
-    version="0.1.5",
+    version="0.1.6",
     description="Scientific matplotlib plot template python module.",
     author="yangsl",
     author_email="3304839708@qq.com",
     url="https://gitee.com/yangsl306/spt",
     packages=find_packages(),
     zip_safe=False,
-    install_requires=["matplotlib>=3.7,<3.8"],
+    install_requires=requirements,
+    python_requires=">3.6",
     platforms=["all"],
 )
