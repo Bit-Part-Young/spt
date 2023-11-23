33d18e1aa0249fba4a5b491d07e203d9cb9e67ab
YSL <3242809681@qq.com>
2023-11-23 11:27:45 +0800

    update example scripts

--- examples/plot_3d.py
+++ examples/plot_3d.py
@@ -4,8 +4,9 @@ from spt.plot_params import set_roman_plot_params
 
 set_roman_plot_params(
     axes_labelpad=15,
-    legend_fontsize=22,
     legend_handletextpad=0.0,
+    legend_fontsize=22,
+    savefig_bbox="standard",
 )
 
 np.random.seed(42)
@@ -14,11 +15,11 @@ x = np.random.rand(num_points)
 y = np.random.rand(num_points)
 z = np.random.rand(num_points)
 
-fig, ax = plt.subplots(subplot_kw={"projection": "3d"}, figsize=(15, 8))
+fig, ax = plt.subplots(subplot_kw={"projection": "3d"}, figsize=(10, 8))
 
 ax.scatter(x, y, z, c="b", marker="o", label=r"$a \, · \, b  \, · \, 10^{-5}$")
 
-ax.set(xlabel="X axis", ylabel="Y axis", zlabel="Z axis")
+ax.set(xlabel="X", ylabel="Y", zlabel="Z")
 ax.legend()
 
 fig.savefig("scatter_3d.png")
