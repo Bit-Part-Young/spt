ebaa7fdfe51d0e7d082a7f39d501480f358f9902
yangsl <3242809681@qq.com>
2023-11-24 12:07:28 +0800

    0.2.0 version

--- setup.py
+++ setup.py
@@ -1,13 +1,12 @@
-from distutils.core import setup
 from pathlib import Path
 from typing import List
 
-from setuptools import find_packages
+from setuptools import find_packages, setup
 
 THIS_DIR = Path(__file__).parent
 
-# with open(THIS_DIR / "README.md", encoding="utf-8") as f:
-#     long_description = f.read()
+with open(THIS_DIR / "docs/description.md", encoding="utf-8") as f:
+    long_description = f.read()
 
 
 def read_requirements(filepath: Path) -> List[str]:
@@ -24,8 +23,10 @@ requirements = read_requirements(THIS_DIR / "requirements.txt")
 
 setup(
     name="spt",
-    version="0.1.9",
+    version="0.2.0",
     description="Scientific matplotlib plot rcParams configuration template python package.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     author="yangsl",
     author_email="3304839708@qq.com",
     url="https://gitee.com/yangsl306/spt",
