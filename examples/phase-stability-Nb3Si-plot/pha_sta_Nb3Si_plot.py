848133f00bad3c0153bf2ae7f9c81585f319b867
yangsl <3242809681@qq.com>
2023-11-26 13:39:51 +0800

    add phase-stability-Nb3Si-plot example

---
+++ examples/phase-stability-Nb3Si-plot/pha_sta_Nb3Si_plot.py
@@ -0,0 +1,77 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+from spt.plot_params import set_plot_params, set_roman_plot_params
+
+subs_ene_fn = "subs_ene_Nb3Si.csv"
+subs_ene_df = pd.read_csv(subs_ene_fn, sep=" ")
+
+equi_site_list = ["Nb_I", "Nb_II", "Nb_III", "Si_I"]
+label_list = ["Nb_{I}", "Nb_{II}", "Nb_{III}", "Si_{I}"]
+
+
+set_roman_plot_params()
+
+fig, ax = plt.subplots()
+
+bar_width = 0.4
+bar_gap = 0.2
+
+
+def pha_sta_ene_plot():
+    # markers = ["o", "s", "^", "v"]
+    solute_list = subs_ene_df.loc[
+        subs_ene_df["solute_site"] == "Nb_I", "solute"
+    ].tolist()
+    x_data = np.arange(len(solute_list)) * 2
+
+    for index, equi_site in enumerate(equi_site_list):
+        # point plot
+        # y_data = ene_data.loc[ene_data["solute_site"] == equi_site, "subs_energy(eV)"]
+        # ax.plot(x_data, y_data, markers[index], label=equi_site)
+
+        # bar plot
+        y_data = subs_ene_df.loc[subs_ene_df["solute_site"] == equi_site, "subs_energy"]
+        ax.bar(
+            x_data + bar_width * (index - 2) + bar_gap,
+            y_data,
+            width=bar_width,
+            label=f"$\mathrm{{{label_list[index]}}}$",
+        )
+
+    ax.axhline(0.0, linestyle="--", c="k")
+
+    ax.set(
+        ylim=(-0.5, 5.0),
+        xticklabels=solute_list,
+        xlabel="Alloying Elements",
+        ylabel="$\mathrm{E_{SS}}$ (eV)",
+    )
+
+    # ax.set_ylim(-0.5, 5.0)
+    # ax.set_xticks(x_data, solute_list)
+    # ax.set_xlabel("Alloying Elements")
+    # ax.set_ylabel("$\mathrm{E_{SS}}$ (eV)")
+    ax.legend()
+
+    fig.savefig("substitution_energy_Nb3Si.png")
+
+    print("\nFigure is generated.")
+
+
+if __name__ == "__main__":
+    pha_sta_ene_plot()
+
+    # latt_cha_list = ["lattice_a(Ang)", "lattice_b(Ang)", "lattice_c(Ang)"]
+    # for latt_cha in latt_cha_list:
+    #     subs_latt_plt(latt_cha=latt_cha)
+
+    #     plt.cla()
+
+    #     print("Figure is generated.")
+    # latt_cha = "lattice_a(Ang)"
+    # latt_cha = "lattice_b(Ang)"
+    # latt_cha = "lattice_c(Ang)"
+    # subs_latt_plt(latt_cha=latt_cha)
+
+    # print("Figure is generated.")
