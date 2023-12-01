27ade3d6576d76475bba8e4085cacbec2fa5ce96
yangsl <3242809681@qq.com>
2023-12-01 11:21:08 +0800

    update setup.py; version 0.2.2

--- setup.py
+++ setup.py
@@ -5,7 +5,8 @@ from setuptools import find_packages, setup
 
 THIS_DIR = Path(__file__).parent
 
-with open(THIS_DIR / "docs/description.md", encoding="utf-8") as f:
+# with open(THIS_DIR / "docs/description.md", encoding="utf-8") as f:
+with open(THIS_DIR / "README.md", encoding="utf-8") as f:
     long_description = f.read()
 
 
@@ -23,16 +24,25 @@ requirements = read_requirements(THIS_DIR / "requirements.txt")
 
 setup(
     name="spt",
-    version="0.2.1",
+    version="0.2.2",
     description="Scientific matplotlib plot rcParams configuration template python package.",
     long_description=long_description,
     long_description_content_type="text/markdown",
     author="yangsl",
     author_email="3304839708@qq.com",
-    url="https://gitee.com/yangsl306/spt",
+    url="https://github.com/Bit-Part-Young/spt",
     packages=find_packages(),
     zip_safe=False,
     install_requires=requirements,
-    python_requires=">3.6",
+    extras_require={
+        "examples": [
+            "pandas",
+            "scikit-learn",
+        ]
+    },
+    python_requires=">=3.8",
     platforms=["all"],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+    ],
 )
